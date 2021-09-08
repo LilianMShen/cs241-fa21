1b25929fe8bd328bafe6ad344d416776701a1b25
Lilian Shen <lmshen2@illinois.edu>
2021-09-08 15:56:46 -0500

    5 steps down

--- perilous_pointers/part2-main.c
+++ perilous_pointers/part2-main.c
@@ -13,5 +13,39 @@
  */
 int main() {
     // your code here
+    // one
+    int val = 81;
+    first_step(val);
+
+    // two
+    val = 132;
+    int* val2 = &val;
+    second_step(val2);
+
+    // three
+    val = 8942;
+    int* val3[] = {val2};
+    double_step(val3);
+
+    // four
+    char* charVal = malloc(sizeof(char));
+    charVal[5] = '\n' + 5;\
+    strange_step(charVal);
+    
+
+    // five
+    void* voival = malloc(sizeof(char));
+    char a = ' ';
+    *(char*)voival = a;
+    empty_step(voival);
+
+    // six
+
+
+    //seven
+
+    
+    free(charVal);
+    free(voival);   
     return 0;
 }
