96931a3f2d57b35e3f0a81d8acc0f70f80bb4c9c
Lilian Shen <lmshen2@illinois.edu>
2021-09-08 17:01:13 -0500

    finished all steps :D

--- perilous_pointers/part2-main.c
+++ perilous_pointers/part2-main.c
@@ -35,17 +35,44 @@ int main() {
 
     // five
     void* voival = malloc(sizeof(char));
-    char a = ' ';
-    *(char*)voival = a;
+    char c = ' ';
+    *(char*)voival = c;
     empty_step(voival);
+    free(voival);
 
     // six
+    *charVal = c;
+    charVal[3] = 'u';
+    voival = charVal;
+    two_step(voival, charVal);
 
 
-    //seven
+    // seven
+    char * charVal2 = charVal + 2;
+    char * charVal3 = charVal2 + 2;
+    three_step(charVal, charVal2, charVal3);
 
-    
-    free(charVal);
-    free(voival);   
+    // eight
+    charVal2[2] = charVal[1] + 8;
+    charVal3[3] = charVal2[2] + 8;
+    step_step_step(charVal, charVal2, charVal3);
+
+    // nine
+    val = 32;
+    it_may_be_odd(charVal, val);
+
+    // ten
+    char str[12] = "CS241,CS241";
+    tok_step((char*) str);
+
+    // eleven
+    void* orange = malloc(sizeof(int*));
+    void* blue = orange;
+    ((char *)blue)[0] = 1;
+    *(int*)orange = 513;
+    the_end(orange, blue);
+
+    free(orange);
+    free(charVal); 
     return 0;
 }
