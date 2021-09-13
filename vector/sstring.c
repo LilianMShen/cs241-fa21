5fd33efc4ae24b9a82a929c65d2830ffcc9dc5a0
Lilian Shen <lmshen2@illinois.edu>
2021-09-12 23:57:22 -0500

    got my own test working, time to check for mem leaks

--- vector/sstring.c
+++ vector/sstring.c
@@ -14,39 +14,102 @@
 
 struct sstring {
     // Anything you want
+    char * string;
 };
 
 sstring *cstr_to_sstring(const char *input) {
     // your code goes here
-    return NULL;
+    sstring * s = malloc(sizeof(sstring));
+    s->string = malloc(strlen(input) + 1);
+    strcpy(s->string, input);
+    return s;
 }
 
 char *sstring_to_cstr(sstring *input) {
     // your code goes here
-    return NULL;
+    char * toReturn = malloc(strlen(input->string) + 1);
+    strcpy(toReturn, input->string);
+    return toReturn;
 }
 
 int sstring_append(sstring *this, sstring *addition) {
     // your code goes here
-    return -1;
+    int n = strlen(this->string);
+    this->string = realloc(this->string, n + (strlen(addition->string) + 1));
+    strcpy(this->string + n, addition->string);
+    n = strlen(this->string);
+    return n;
 }
 
 vector *sstring_split(sstring *this, char delimiter) {
     // your code goes here
-    return NULL;
+    vector * toReturn = string_vector_create();
+    char * iterator = this->string;
+    int i = 0;
+    int j = 0;
+    while(*iterator) {
+        if (*iterator == delimiter) {
+            vector_push_back(toReturn, sstring_slice(this, i, j));
+            i = j + 1;
+        }
+        j++;
+        iterator++;
+    }
+    vector_push_back(toReturn, sstring_slice(this, i, j));
+    // strcpy(copy, this->string);
+    // char * token = strtok(copy, &delimiter);
+    // while (token) {
+    //     vector_push_back(toReturn, token);
+    //     token = strtok(NULL, &delimiter);
+    // }
+    // vector_push_back(toReturn, NULL);
+    // free(copy);
+    return toReturn;
 }
 
 int sstring_substitute(sstring *this, size_t offset, char *target,
                        char *substitution) {
     // your code goes here
+    char * iterator = &(this->string[offset]);
+    size_t len = strlen(target);
+    size_t i = offset;
+    int flagFound = 0;
+    char * copy;
+    while(*iterator) {
+        if(strncmp(iterator, target, len) == 0) {
+            flagFound = 1;
+            break;
+        }
+        i++;
+        iterator++;
+    }
+    if (flagFound) {
+        copy = sstring_slice(this, 0, i);
+        strncpy(&copy[i], substitution, strlen(substitution));
+        strcpy(&copy[i + strlen(substitution)], &this->string[i + strlen(target)]);
+        // string_destructor(this->string);
+        // this->string = malloc(strlen(copy) + 1);
+        strcpy(this->string, copy);
+        free(copy);
+        return 0;
+        // copy = realloc(copy, strlen(this->string) + 1 + (strlen(*substitution) - strlen(*target)));
+    }
     return -1;
 }
 
 char *sstring_slice(sstring *this, int start, int end) {
     // your code goes here
-    return NULL;
+    int size = end - start;
+    char * toReturn = malloc((sizeof(char) * size) + 1);
+    for (int i = 0; i < size; i++) {
+        toReturn[i] = this->string[start + i];
+    }
+    toReturn[size] = '\0';
+    return toReturn;
 }
 
 void sstring_destroy(sstring *this) {
     // your code goes here
+    free(this->string);
+    free(this);
 }
--- vector/sstring_test.c
+++ vector/sstring_test.c
@@ -3,8 +3,87 @@
  * CS 241 - Fall 2021
  */
 #include "sstring.h"
+#include <string.h>
+#include <assert.h>
 
 int main(int argc, char *argv[]) {
-    // TODO create some tests
+    // Testing basic constructors
+    printf("Testing basic constructors... ");
+    sstring *str = cstr_to_sstring("testing123");
+    char *cstr = sstring_to_cstr(str);
+    assert(strcmp(cstr, "testing123") == 0);
+    // Destroy memory associated with return value
+    string_destructor((void*)cstr);
+    printf("done\n");
+
+    // Testing append
+    printf("Testing basic append... ");
+    sstring *str2 = cstr_to_sstring(". okay now what?");
+    assert(sstring_append(str, str2) == 26);
+    char *append1 = sstring_to_cstr(str);
+    char *append2 = sstring_to_cstr(str2);
+    assert(strcmp(append1, "testing123. okay now what?") == 0);
+    assert(strcmp(append2, ". okay now what?") == 0);
+    string_destructor((void*)append1);
+    string_destructor((void*)append2);
+    printf("done\n");
+
+    // Test destructor
+    printf("Testing destructor... ");
+    sstring_destroy(str2);
+    printf("done\n");
+
+    // Test slice
+    printf("Testing slice... ");
+    char *slice_result = sstring_slice(str, 1, 3);
+    assert(strcmp(slice_result, "es") == 0);
+    string_destructor((void*)slice_result);
+    printf("done\n");
+
+    // Test split
+    printf("Testing split... ");
+    vector *split_result = sstring_split(str, '.');
+    
+    assert(strcmp(vector_get(split_result, 0), "testing123") == 0);
+    assert(strcmp(vector_get(split_result, 1), " okay now what?") == 0);
+    vector_destroy(split_result);
+
+    split_result = sstring_split(str, 'x');
+    assert(strcmp(vector_get(split_result, 0), "testing123. okay now what?") == 0);
+    vector_destroy(split_result);
+
+    split_result = sstring_split(str, '?');
+    assert(strcmp(vector_get(split_result, 1), "") == 0);
+    vector_destroy(split_result);
+
+    printf("done\n");
+
+    // Test substitute
+    printf("Testing substitute... ");
+    assert(sstring_substitute(str, 0, "lol", "wut?") == -1);
+    assert(sstring_substitute(str, 0, "testing", "workkking") == 0);
+
+    char* sub_result = sstring_to_cstr(str);
+    assert(strcmp(sub_result, "workkking123. okay now what?") == 0);
+    string_destructor((void*)sub_result);
+    assert(sstring_substitute(str, 1, "workkking", "slacccccing") == -1);
+    assert(sstring_substitute(str, 1, "kkk", "cc") == 0);
+    sub_result = sstring_to_cstr(str);
+    assert(strcmp(sub_result, "worccing123. okay now what?") == 0);
+    string_destructor((void*)sub_result);
+
+    assert(sstring_substitute(str, 5, "okay", "ummm") == 0);
+    sub_result = sstring_to_cstr(str);
+    assert(strcmp(sub_result, "worccing123. ummm now what?") == 0);
+    string_destructor((void*)sub_result);
+    printf("done\n");
+    
+    
+
+    sstring_destroy(str);
+    str = NULL;
+
+    printf("All tests passed!\n");
+
     return 0;
 }
