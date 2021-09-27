ea5918e973afe40b7874daff2cec64ebfc0c8872
Lilian Shen <lmshen2@illinois.edu>
2021-09-26 23:14:53 -0500

    i have a massive headache

--- shell/shell.c
+++ shell/shell.c
@@ -13,6 +13,8 @@
 #include <signal.h>
 #include <sys/stat.h>
 #include <sys/wait.h>
+#include <fcntl.h>
+#include <errno.h>
 
 typedef struct process {
     char *command;
@@ -20,7 +22,7 @@ typedef struct process {
 } process;
 
 // to get rid of implicit declaration
-void runCmd(vector*h, char*line);
+void runCmd(vector*h, vector * pids, char*line);
 
 // handles ctrl c
 void handle_sigint(int sig) {
@@ -63,7 +65,7 @@ char * getInput(void) {
 }
 
 // logic for handling built in functions
-int isBuiltIn(vector * v, vector * h) {
+int isBuiltIn(vector * v, vector * h, vector * pids) {
     if (vector_empty(v)) {
         return 0;
     }
@@ -98,7 +100,7 @@ int isBuiltIn(vector * v, vector * h) {
         char * l = vector_get(h, index);
         print_history_line(index, l);
         vector_push_back(h, l);
-        runCmd(h, l);
+        runCmd(h, pids, l);
         return 1;
     } else if (firstWord[0] == '!') {
         vector_erase(h, vector_size(h) - 1);
@@ -107,7 +109,7 @@ int isBuiltIn(vector * v, vector * h) {
         if (strlen(firstWord) == 1) {
             print_history_line(0, l);
             vector_push_back(h, l);
-            runCmd(h, l);
+            runCmd(h, pids, l);
             return 1;
         }
         char prefix = firstWord[1];
@@ -117,7 +119,7 @@ int isBuiltIn(vector * v, vector * h) {
             if (l[0] == prefix) {
                 print_history_line(i, l);
                 vector_push_back(h, l);
-                runCmd(h, l);
+                runCmd(h, pids, l);
                 return 1;
             }
         }
@@ -128,7 +130,7 @@ int isBuiltIn(vector * v, vector * h) {
 }
 
 // logic for handling fork exec wait functions
-int isNotBuiltIn(vector* v, vector* h) {
+int isNotBuiltIn(vector* v, vector* h, vector * pids, int isBackground, int redirectFlag, vector * splicedLineTwo) {
     if (vector_empty(v)) {
         return 0;
     }
@@ -140,29 +142,58 @@ int isNotBuiltIn(vector* v, vector* h) {
     }
 
     char** argv = (char**) vector_front(v);
+    pid_t curpid = getpid();
 
+    int filefd = 0;
     if (childPid == 0) { // do child function
         char path[strlen(argv[0]) + 6];
         strcpy(path, "/bin/");
         strcat(path, argv[0]);
-        pid_t curpid = getpid();
         print_command_executed(curpid);
+
+        if (redirectFlag != 0) {
+            char * fileName = vector_get(splicedLineTwo, 0);
+            // output
+            if (redirectFlag == 2) filefd = open(fileName, O_CREAT|O_WRONLY, 0777);
+            // append
+            if (redirectFlag == 1) filefd = open(fileName, O_CREAT|O_APPEND|O_WRONLY, 0777);
+            // input
+            if (redirectFlag == 3) filefd = open(fileName, O_RDONLY, 0777);
+            if (filefd == -1) {
+                print_redirection_file_error();
+                exit(1);
+                return -1;
+            }
+            dup2(filefd, 1);
+        }
+
         if (execvp(path , argv) == -1) {
             print_exec_failed(vector_get(h, vector_size(h) - 1));
+            exit(1);
             return -1;
         }
     }
 
-    if (childPid > 0) { // do parent function
+    // aka foreground processes
+    if (childPid > 0 && isBackground == 0) { // do parent function
         int status;
         wait(&status);
+        // DO NOT UNCOMMENT, THIS IS A FORK BOMB
+        // if (redirectFlag != 0) {
+        //     close(filefd);
+        // }
         if (!WIFEXITED(status)) {
             print_exec_failed(vector_get(h, vector_size(h) - 1));
+            exit(1);
             return -1;
         }
+    } else if (childPid > 0 && isBackground == 1) {
+        // printf("hit background process \n");
+        int pgid = setpgid(0, 0);
+        vector_push_back(pids, curpid);
     }
 
-    return 1;
+    return 0;
 }
 
 // logic for finding if user input contains an operator, returns index of operator
@@ -172,6 +203,11 @@ int containsOperator(vector* splicedLine) {
         if ((strcmp(word, "||") == 0) || (strcmp(word, "&&") == 0)) {
             return i;
         }
+
+        if ((strcmp(word, ">>") == 0) || (strcmp(word, ">") == 0) || (strcmp(word, "<") == 0)) {
+            return i;
+        }
+
         if (word[strlen(word) - 1] == ';') {
             word[strlen(word) - 1] = '\0';
             printf("%s", word);
@@ -190,11 +226,31 @@ void newSplicedLine(vector* splicedLineTwo, vector* splicedLine, int index) {
     }
 }
 
+// void overwriteFile(vector * pids) {
+//     FILE * file;
+// }
+
+// void killChild(vector * v, vector * pids, char * line) {
+    
+//     if (vector_size(v) < 2) ;
+//     pid_t pid = vector_get(v, 1);
+//     kill(pid, SIGKILL);
+//     if (errno == ESRCH) {
+//         print_no_process_found(pid); 
+//     } else {
+//         print_killed_process(pid, line);
+//     }
+// }
+
 // runs the command(s) given a line
-void runCmd(vector* h, char*line) {
+void runCmd(vector* h, vector * pids, char*line) {
     int orFlag = 0;
     int andFlag = 0;
     int semiFlag = 0;
+    int redirectFlag = 0;
+    // int outputFlag = 0;
+    // int appendFlag = 0;
+    // int inputFlag = 0;
     int suc1a = 0;
     int suc2a = 0;
 
@@ -204,21 +260,49 @@ void runCmd(vector* h, char*line) {
     vector * splicedLineTwo = string_vector_create();
     if (operatorIndex != -1) {
         newSplicedLine(splicedLineTwo, splicedLine, operatorIndex);
-        if (strcmp(vector_get(splicedLineTwo, 0), "||") == 0) {
+        char * operator = vector_get(splicedLineTwo, 0);
+
+        if (strcmp(operator, "||") == 0) {
             orFlag = 1;
             vector_erase(splicedLineTwo, 0);
-        } else if (strcmp(vector_get(splicedLineTwo, 0), "&&") == 0) {
+        } else if (strcmp(operator, "&&") == 0) {
             andFlag = 1;
             vector_erase(splicedLineTwo, 0);
+            
+        } else if (strcmp(operator, ">>") == 0) {
+            // appendFlag = 1;
+            redirectFlag = 1;
+            vector_erase(splicedLineTwo, 0);
+            
+        } else if (strcmp(operator, ">") == 0) {
+            // outputFlag = 1;
+            redirectFlag = 2;
+            vector_erase(splicedLineTwo, 0);
+            
+        } else if (strcmp(operator, "<") == 0) {
+            // inputFlag = 1;
+            redirectFlag = 3;
+            vector_erase(splicedLineTwo, 0);
+            
         } else {
             semiFlag = 1;
         }
     }
 
-    suc1a = isBuiltIn(splicedLine, h);
-    if (suc1a == 0) {
-        suc2a = isNotBuiltIn(splicedLine, h);
+    // checking for background processes
+    int isBackground = 0;
+    char * lastWord = vector_get(splicedLine, vector_size(splicedLine) - 1);
+    if (lastWord[0] == '&') {
+        isBackground = 1;
+        vector_erase(splicedLine, vector_size(splicedLine) - 1);
     }
+
+    suc1a = isBuiltIn(splicedLine, h, pids);
+    if (suc1a == 0) {
+        suc2a = isNotBuiltIn(splicedLine, h, pids, isBackground, redirectFlag, splicedLineTwo);
+    }
+
+
     int suc1 = 0;
     if (suc1a == 1 || suc2a == 1) {
         suc1 = 1;
@@ -229,19 +313,19 @@ void runCmd(vector* h, char*line) {
     int suc1b = 0;
     int suc2b = 0;
     if ((suc1 == 1) && (andFlag == 1)) {
-        suc1b = isBuiltIn(splicedLineTwo, h);
+        suc1b = isBuiltIn(splicedLineTwo, h, pids);
         if (suc1b == 0) {
-            suc2b = isNotBuiltIn(splicedLineTwo, h);
+            suc2b = isNotBuiltIn(splicedLineTwo, h, pids, isBackground, redirectFlag, splicedLineTwo);
         }
     } else if ((suc1 == -1) && (orFlag == 1)) {
-        suc1b = isBuiltIn(splicedLineTwo, h);
+        suc1b = isBuiltIn(splicedLineTwo, h, pids);
         if (suc1b == 0) {
-            suc2b = isNotBuiltIn(splicedLineTwo, h);
+            suc2b = isNotBuiltIn(splicedLineTwo, h, pids, isBackground, redirectFlag, splicedLineTwo);
         }
     } else if (semiFlag == 1) {
-        suc1b = isBuiltIn(splicedLineTwo, h);
+        suc1b = isBuiltIn(splicedLineTwo, h, pids);
         if (suc1b == 0) {
-            suc2b = isNotBuiltIn(splicedLineTwo, h);
+            suc2b = isNotBuiltIn(splicedLineTwo, h, pids, isBackground, redirectFlag, splicedLineTwo);
         }
     }
     
@@ -250,7 +334,7 @@ void runCmd(vector* h, char*line) {
     vector_destroy(splicedLineTwo);
 }
 
-int runUserInput(vector * h) {
+int runUserInput(vector * h, vector * pids) {
     pid_t parentPid = getpid();
     char directory[200];
     char *line;
@@ -263,35 +347,30 @@ int runUserInput(vector * h) {
         // stick this somewhere lmfao
         signal(SIGINT, &handle_sigint);
 
+        // // kill zombie children
+        // waitpid(-1, NULL, WNOHANG);
+
         line = getInput();
 
         int eofFlag = feof(stdin);
         if (eofFlag != 0) {
             free(line);
             exitFlag = -1;
-            return -1;
+            break;
         }
 
         if (strcmp(line, "exit") == 0) {
             free(line);
             exitFlag = -1;
-            return -1;
+            break;
         }
         
         vector_push_back(h, line);
-
-        // catch background process
-        if (line[strlen(line) - 2] == ' ' && line[strlen(line) - 1] == '&') {
-            exitFlag = runUserInput(h);
-            // if (exitFlag == -1) {
-            //     return exitFlag;
-            // }
-        }
         
-        runCmd(h, line);
+        runCmd(h, pids, line);
         free(line);
     }
-    return 0;
+    return -1;
 }
 
 int shell(int argc, char *argv[]) {
@@ -299,6 +378,7 @@ int shell(int argc, char *argv[]) {
     char directory[200];
     getcwd(directory, 200);
     vector * h = string_vector_create();
+    vector * pids = int_vector_create();
     int hFlag = 0;
     char* hFile;
     int fFlag = 0;
@@ -348,13 +428,13 @@ int shell(int argc, char *argv[]) {
             str[strlen(str) - 1] = '\0';
             // printf("%s\n", str);
             vector_push_back(h, str);
-            runCmd(h, str);
+            runCmd(h, pids, str);
         }
         free(str);
         fclose(fptr);
         fFlag = 0;
     } else {
-        runUserInput(h);
+        runUserInput(h, pids);
     }
 
     // use fopen and fclose to store in history.text
@@ -375,7 +455,18 @@ int shell(int argc, char *argv[]) {
         fclose(hptr);
     }
 
+
+    // kill remaining pids
+    pid_t kidpid;
+    int status;
+    while (vector_size(pids) > 0) {
+        int * pid = vector_get(pids, vector_size(pids) - 1);
+        kidpid = waitpid(*pid, &status, WNOHANG);
+        vector_erase(pids, vector_size(pids) - 1);
+    }
+
     vector_destroy(h);
+    vector_destroy(pids);
     exit(0);
     return 0;
 }
