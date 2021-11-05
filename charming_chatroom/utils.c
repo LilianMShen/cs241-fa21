a5c8d136e666cfffa670e43b133535be5d2c1680
Lilian Shen <lmshen2@illinois.edu>
2021-11-04 22:20:57 -0500

    oops changed the wrong one

--- charming_chatroom/utils.c
+++ charming_chatroom/utils.c
@@ -36,7 +36,7 @@ ssize_t get_message_size(int socket) {
 // You may assume size won't be larger than a 4 byte integer
 ssize_t write_message_size(size_t size, int socket) {
     int32_t s = htonl((uint32_t)size);
-    ssize_t ret = write(socket, &s, sizeof(s));
+    ssize_t ret = write_all_to_socket(socket, &s, sizeof(s));
     if (ret < 0) {
         perror(NULL);
         exit(1);
@@ -66,7 +66,7 @@ ssize_t read_all_from_socket(int socket, char *buffer, size_t count) {
 }
 
 ssize_t write_all_to_socket(int socket, const char *buffer, size_t count) {
-    ssize_t ret = write_all_to_socket(socket, buffer, count);
+    ssize_t ret = write(socket, buffer, count);
     if (ret < 0) {
         perror(NULL);
         exit(1);
